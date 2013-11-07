a779156268d2dabb9953bb514a9c1db2d9612583
Vinayak Sathyamoorthy <vsathyamoorthy2014@kellogg.northwestern.edu>
2013-11-07 17:03:04 -0600

    Extra Credit Submission

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 AddressToCoords::Application.routes.draw do
   get("/coords", { :controller => "addresses", :action => "fetch_coordinates" })
+  get("/convert", {:controller => "addresses", :action => "convert_complete"})
+  get("/new_address", { :controller => "addresses", :action => "address_form"})
 end
